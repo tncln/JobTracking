bf435d7b0c9104eaaddf2ae5095e5cd231f1311b
JobTracking <tuncalin@outlook.com>
2021-09-21 23:57:01 +0300

    Entity Add

---
+++ JobTracking.Entity/Concrete/Calisma.cs
@@ -0,0 +1,18 @@
+﻿using JobTracking.Entity.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobTracking.Entity.Concrete
+{
+    public class Calisma:ITablo
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Aciklaması { get; set; }
+        public DateTime AciklamaTarihi { get; set; }
+
+        public int KullaniciId { get; set; }
+        public Kullanici Kullanici { get; set; }
+    }
+}
