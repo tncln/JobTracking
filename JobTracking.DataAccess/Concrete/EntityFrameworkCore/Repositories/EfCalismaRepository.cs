55c830d0e991cf704bfb92b2c76c26edb4a8648f
JobTracking <tuncalin@outlook.com>
2021-10-14 18:49:12 +0300

    IGenericRepository implementasyonunun yapılması

--- JobTracking.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfCalismaRepository.cs
+++ JobTracking.DataAccess/Concrete/EntityFrameworkCore/Repositories/EfCalismaRepository.cs
@@ -8,51 +8,8 @@ using System.Text;
 
 namespace JobTracking.DataAccess.Concrete.EntityFrameworkCore.Repositories
 {
-    public class EfCalismaRepository : ICalismaDal
+    public class EfCalismaRepository :IGenericRepository<Calisma>, ICalismaDal
     {
-        public List<Calisma> Getirhepsi()
-        {
-            using (var context=new TodoContext())
-            {
-                return context.Calismalar.ToList();
-            }
-        }
-
-        public Calisma GetirIdile(int id)
-        {
-            using (var context = new TodoContext())
-            {
-                return context.Calismalar.Find(id);
-            }
-        }
-
-        public void Guncelle(Calisma tablo)
-        {
-            using (var context = new TodoContext())
-            {
-                //Diğer yöntem sıkıntısı: tüm kolonları günceller. Gereksiz performans harcaması  
-                //context.Calismalar.Update(tablo);
-                context.Entry(tablo).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-
-        public void Kaydet(Calisma tablo)
-        {
-            using (var context = new TodoContext())
-            {
-                context.Calismalar.Add(tablo);
-                context.SaveChanges();
-            }
-        }
-
-        public void Sil(Calisma tablo)
-        {
-            using (var context = new TodoContext())
-            {
-                context.Calismalar.Remove(tablo);
-                context.SaveChanges();
-            }
-        }
+       
     }
 }
