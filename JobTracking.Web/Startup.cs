c3316c74398561f5e282e1c42e0841bc4229f138
JobTracking <tuncalin@outlook.com>
2021-10-14 22:11:07 +0300

    MVC implementasyonu

--- JobTracking.Web/Startup.cs
+++ JobTracking.Web/Startup.cs
@@ -23,6 +23,7 @@ namespace JobTracking.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +46,10 @@ namespace JobTracking.Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}"
+                    );
             });
         }
     }
