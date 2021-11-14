a067a232d0f103613f698ad5a2748fca24e5bf86
JobTracking <tuncalin@outlook.com>
2021-11-14 22:27:31 +0300

    stringBuilder ve string ile Custom tag helper yazımı

--- JobTracking.Web/TagHelpers/Paragraf.cs
+++ JobTracking.Web/TagHelpers/Paragraf.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JobTracking.Web.TagHelpers
@@ -17,13 +18,27 @@ namespace JobTracking.Web.TagHelpers
             //tagbuilder
             //stringbuilder
 
-
+            #region TagBuilder ile Custom tag helper
             //output
             //<p> <b>Adem Tunçalın</b> </p>
-            var tagBuilder = new TagBuilder("p");
-            tagBuilder.InnerHtml.AppendHtml("<b>Adem Tunçalın</b>");
-            output.Content.SetHtmlContent(tagBuilder);
+            //var tagBuilder = new TagBuilder("p");
+            //tagBuilder.InnerHtml.AppendHtml("<b>Adem Tunçalın</b>");
+            //output.Content.SetHtmlContent(tagBuilder);
+
+            #endregion
+
+            #region string builder ile tag helper 
+            //var stringBuilder = new StringBuilder();
+            //stringBuilder.AppendFormat("<p><b> {0} </b></p>", "Adem Tunçalın");
+            //output.Content.SetHtmlContent(stringBuilder.ToString());
+            #endregion
+            string disardangelen = "Adem Tunçalın";
+            string data = string.Empty;
+            data="<p><b>"+disardangelen+"</b></p>";
+            output.Content.SetHtmlContent(data);
             base.Process(context, output);
+
+
         }
     }
 }
