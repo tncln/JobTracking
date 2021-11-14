7f1c827be7330d2bf50088975b114fcd0ca37e61
JobTracking <tuncalin@outlook.com>
2021-11-14 22:16:41 +0300

    custom tag helper --> tagbuilder ile

---
+++ JobTracking.Web/TagHelpers/Paragraf.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTracking.Web.TagHelpers
+{
+    [HtmlTargetElement("adem")]
+    public class Paragraf:TagHelper
+    {
+        
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //string data 
+            //tagbuilder
+            //stringbuilder
+
+
+            //output
+            //<p> <b>Adem Tunçalın</b> </p>
+            var tagBuilder = new TagBuilder("p");
+            tagBuilder.InnerHtml.AppendHtml("<b>Adem Tunçalın</b>");
+            output.Content.SetHtmlContent(tagBuilder);
+            base.Process(context, output);
+        }
+    }
+}
