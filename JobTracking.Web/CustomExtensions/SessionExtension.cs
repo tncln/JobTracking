59881dd61cc59fde21d401297116f936c2b47ca9
JobTracking <tuncalin@outlook.com>
2021-11-18 22:22:21 +0300

    Custom Session Object extensions

---
+++ JobTracking.Web/CustomExtensions/SessionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTracking.Web.CustomExtensions
+{
+    public static class SessionExtension
+    {
+        //session nesnesi geliştirilerek object destekler 
+        public static void SetObject(this ISession session,string key, object deger)
+        {
+            string gelenData = JsonConvert.SerializeObject(deger);
+            session.SetString(key, gelenData);
+        }
+        public static object GetObject(this ISession session,string key)
+        {
+            var gelenData= session.GetString(key);
+            if (gelenData != null)
+            {
+               return JsonConvert.DeserializeObject<object>(gelenData);
+            }
+            return null;
+        }
+    }
+}
