99f306ee50308d983ee8e6c821f53cde0d11b54e
JobTracking <tuncalin@outlook.com>
2022-01-07 01:29:01 +0300

    Gorevleri Listeleme

---
+++ JobTracking.UI/Areas/Admin/Controllers/GorevController.cs
@@ -0,0 +1,43 @@
+﻿using JobTracking.Business.Interfaces;
+using JobTracking.Entity.Concrete;
+using JobTracking.UI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTracking.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GorevController : Controller
+    {
+        IGorevService _gorevService;
+        public GorevController(IGorevService gorevService)
+        {
+            _gorevService = gorevService;
+        }
+        public IActionResult Index()
+        {
+            TempData["Active"] = "gorev";
+            List<Gorev> gorevler = _gorevService.Getirhepsi();
+            List<GorevListViewModel> models = new List<GorevListViewModel>();
+            foreach (var item in gorevler)
+            {
+                GorevListViewModel model = new GorevListViewModel()
+                {
+                    Aciklama = item.Aciklama,
+                    AciklamaTarihi = item.AciklamaTarihi,
+                    Aciliyet = item.Aciliyet,
+                    AciliyetId = item.AciliyetId,
+                    Ad = item.Ad,
+                    Durum = item.Durum,
+                    Id = item.Id
+                };
+                models.Add(model);
+            }
+
+            return View(models);
+        }
+    }
+}
