5c20481e6f4f72c6e2045f0abff7dbdbef0daeec
JobTracking <tuncalin@outlook.com>
2022-01-06 22:50:05 +0300

    Aciliyetlerin Listelenmesi

---
+++ JobTracking.UI/Areas/Admin/Controllers/AciliyetController.cs
@@ -0,0 +1,35 @@
+﻿using JobTracking.Business.Interfaces;
+using JobTracking.Entity.Concrete;
+using JobTracking.UI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTracking.UI.Areas.Admin
+{
+    [Area("Admin")]
+    public class AciliyetController : Controller
+    {
+        private readonly IAciliyetService _aciliyetService;
+        public AciliyetController(IAciliyetService aciliyetService)
+        {
+            _aciliyetService = aciliyetService;
+        }
+        public IActionResult Index()
+        {
+            List<Aciliyet> aciliyetler= _aciliyetService.Getirhepsi();
+
+            List<AciliyetListViewModel> model = new List<AciliyetListViewModel>();
+            foreach (var item in aciliyetler)
+            {
+                AciliyetListViewModel aciliyetModel = new AciliyetListViewModel();
+                aciliyetModel.Id = item.Id;
+                aciliyetModel.Tanim = item.Tanim;
+                model.Add(aciliyetModel);
+            }
+            return View(model);
+        }
+    }
+}
