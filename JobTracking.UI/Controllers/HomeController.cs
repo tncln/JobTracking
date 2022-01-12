c9329b58b52638f4e8cd902be549c8e686c31321
JobTracking <tuncalin@outlook.com>
2022-01-13 00:30:26 +0300

    Kullanıcı Kayıt İşlemleri

--- JobTracking.UI/Controllers/HomeController.cs
+++ JobTracking.UI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using JobTracking.Business.Interfaces;
+using JobTracking.Entity.Concrete;
+using JobTracking.UI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,10 +12,12 @@ namespace JobTracking.UI.Controllers
 {
     public class HomeController : Controller
     {
-        IGorevService _gorevService;
-        public HomeController(IGorevService gorevService)
+        private readonly IGorevService _gorevService;
+        private readonly UserManager<AppUser> _userManager;
+        public HomeController(IGorevService gorevService, UserManager<AppUser> userManager)
         {
             _gorevService = gorevService;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -21,6 +26,42 @@ namespace JobTracking.UI.Controllers
         public IActionResult KayitOl()
         {
             return View();
+        }
+        [HttpPost]
+        public async  Task<IActionResult> KayitOl(AppUserAddViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    Name = model.Name,
+                    Surname = model.Surname
+                };
+               var result=  await _userManager.CreateAsync(user,model.Password);
+
+                if (result.Succeeded)
+                {
+                   var addRoleResult= await _userManager.AddToRoleAsync(user, "Member");
+                    if (addRoleResult.Succeeded)
+                    {
+                        return RedirectToAction("GirisYap");
+                    }
+                    else
+                    {
+                        foreach (var item in addRoleResult.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
