67f0f21f607f5f552bf2825b73b908298f61dcf1
JobTracking <tuncalin@outlook.com>
2022-02-09 23:07:38 +0300

    Profil Güncelleme Formunun oluşturulması

---
+++ JobTracking.UI/Areas/Admin/Controllers/ProfilController.cs
@@ -0,0 +1,34 @@
+﻿using JobTracking.Entity.Concrete;
+using JobTracking.UI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTracking.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ProfilController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public ProfilController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var appUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserListViewModel model = new AppUserListViewModel();
+            model.Id = appUser.Id;
+            model.Name = appUser.Name;
+            model.SurName = appUser.Surname;
+            model.Picture = appUser.Picture;
+
+            return View(model);
+        }
+    }
+}
