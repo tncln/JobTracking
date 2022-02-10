bc1274af97973be973b56005d412a03116d23a51
JobTracking <tuncalin@outlook.com>
2022-02-11 01:19:04 +0300

    Member Layout Ayağa kaldırılması

--- JobTracking.UI/ViewComponents/Wrapper.cs
+++ JobTracking.UI/ViewComponents/Wrapper.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace JobTracking.UI.Areas.Admin.ViewComponents
+namespace JobTracking.UI.ViewComponents
 {
     public class Wrapper:ViewComponent
     {
@@ -18,14 +18,20 @@ namespace JobTracking.UI.Areas.Admin.ViewComponents
         }
         public IViewComponentResult Invoke()
         {
-           var user= _userManager.FindByNameAsync(User.Identity.Name).Result;
+            var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
             AppUserListViewModel model = new AppUserListViewModel();
             model.Id = user.Id;
             model.Name = user.Name;
             model.Picture = user.Picture;
             model.SurName = user.Picture;
             model.Email = user.Email;
-            return View(model);
+            var roles = _userManager.GetRolesAsync(user).Result;
+            if (roles.Contains("Admin"))
+            {
+                return View(model);
+            }
+            return View("Member", model);
+            
         }
     }
 }
