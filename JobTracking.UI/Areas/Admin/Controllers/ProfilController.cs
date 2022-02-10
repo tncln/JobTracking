33a48197c988e7a15b0d0623e9d9659c815629f5
JobTracking <tuncalin@outlook.com>
2022-02-10 22:44:53 +0300

    Resim Güncelleme ve Profil Güncelleme İşlemleri

--- JobTracking.UI/Areas/Admin/Controllers/ProfilController.cs
+++ JobTracking.UI/Areas/Admin/Controllers/ProfilController.cs
@@ -1,10 +1,12 @@
 ﻿using JobTracking.Entity.Concrete;
 using JobTracking.UI.Areas.Admin.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -29,6 +31,39 @@ namespace JobTracking.UI.Areas.Admin.Controllers
             model.SurName = appUser.Surname;
             model.Picture = appUser.Picture;
 
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserListViewModel model, IFormFile resim)
+        {
+            if (ModelState.IsValid)
+            {
+                var guncellenecekKullanici = _userManager.Users.FirstOrDefault(x => x.Id == model.Id);
+                if (resim != null)
+                {
+                    string uzanti = Path.GetExtension(resim.FileName);
+                    string resimAd = Guid.NewGuid() + uzanti;
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/" + resimAd);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await resim.CopyToAsync(stream);
+                    }
+                    guncellenecekKullanici.Picture = resimAd;
+                }
+                guncellenecekKullanici.Name = model.Name;
+                guncellenecekKullanici.Surname = model.SurName;
+                guncellenecekKullanici.Email = model.Email;
+                var result = await _userManager.UpdateAsync(guncellenecekKullanici);
+                if (result.Succeeded)
+                {
+                    TempData["message"] = "Güncelleme İşleminiz Başarı ile Gerçekleştirildi.";
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
             return View(model);
         }
     }
